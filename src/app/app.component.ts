44192ef43a28f744012e2df8a66eead0ec5ccf47
gesielbr <gesiel.br@gmail.com>
2024-07-05 14:10:23 -0300

    dinamic forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'formsWithDinamicSelection';
+  user: string = '';
+  email: string = '';
+  city: string = '';
+
+  countries = [
+    { name: 'USA', value: 'usa' },
+    { name: 'Canada', value: 'canada' },
+    { name: 'UK', value: 'uk' },
+  ];
+
+  cities: { [key: string]: string[] } = {
+    usa: ['New York', 'Los Angeles', 'Chicago'],
+    canada: ['Toronto', 'Vancouver', 'Montreal'],
+    uk: ['London', 'Manchester', 'Birmigham'],
+  };
+
+  getCitiesByCountry(country: string): string[] {
+    return this.cities[country] || [];
+  }
+
+  onSubmit(myForm: NgForm) {
+    if (myForm.valid) {
+      const formData = {
+        user: this.user,
+        email: this.email,
+        country: this.selectedCountry,
+        city: this.city,
+      };
+      console.log('Form Submitted', formData);
+    } else {
+      alert('Please fill up the fields');
+    }
+  }
+
+  selectedCountry = this.countries[0].value;
 }
